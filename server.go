6ec393af22b1161ab715ab6d41c319155739adb6
root <root@bogon.(none)>
2015-09-22 17:08:05 +0800

    complete server

--- server.go
+++ server.go
@@ -1,5 +1,48 @@
 package M
 
 import (
+	"fmt"
 	"net"
+	"os"
 )
+
+const (
+	addr  = ":8080"
+	proto = "tcp"
+)
+
+var (
+	listener net.Listener
+)
+
+func init() {
+	var err error
+	listener, err = net.Listen(proto, addr)
+	if checkErr(err) {
+		fmt.Println("fail  to listen")
+		os.Exit(1)
+	}
+}
+func serve() {
+	for {
+		c, err := listener.Accept()
+		if checkErr(err) {
+			continue
+		}
+		go handle(c)
+	}
+}
+
+var r_test = NewRoom()
+
+func handle(c net.Conn) {
+	u := NewUnit(c)
+	r_test.AddUnit(u)
+	for {
+		a, _ := u.Read()
+		if a == nil {
+			continue
+		}
+		r_test.BroadcastByUnit(u, a)
+	}
+}
